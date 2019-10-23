faf6bbfa1c8159a7967f45a04c7382adfb9e41bd
Zhiliang <Zhiliang.Peng@handeson.com>
2019-10-23 16:38:05 +0800

    使用child_process，控制命令

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
-console.log(process.cwd(), 'cwd');
+const pa = process.argv;
+const argv = pa[pa.length - 1];
+const { spawn } = require('child_process');
+const path = require('path');
 
-require('../server');
+switch(argv) {
+    case 'dev':
+        require('../server');
+        break;
+    case "build":
+        const compile = spawn(
+            process.platform === "win32" ? "npm.cmd" : "npm", 
+            ['run', 'prod'],
+            {
+                stdio: 'inherit',cwd: path.resolve(__dirname, '..')
+            }
+        );
+        break;
+    default: 
+        require('../repo');
+};

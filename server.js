aeb42784506e7702cfdb2f6afaa334b0c6858c18
pzl <pzl1026@aliyun.com>
2020-06-13 17:43:51 +0800

    tijiao

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const compiler = webpack(config);
 const conf = require('./bin/conf');
 const CWD= process.cwd();
 // const mockFiles = fs.readdirSync(path.resolve(CWD, './mock'));
-
+console.log(222)
 const options = {
     watchOptions: {
       ignored: /node_modules/,
